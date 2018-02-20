ca8b50c308bb5f64d2ff16fa41311f549b778798
Yurisa <779509411@qq.com>
2018-02-20 13:17:45 +0800

    封装Promise方法 git commit -m 封装Promise方法

---
+++ Promise.js
@@ -0,0 +1,27 @@
+/**
+ * 将异步函数封装成Promise
+ */
+
+var promisify = function (method) {
+    return function (ctx) {
+    	//获取method调用的需要参数
+        var args = Array.prototype.slice.call(arguments, 1);
+
+        //返回一个新的Promise对象
+        return new Promise(function (resolve, reject) {
+        	//除了函数传入的参数以外还需要一个callback函数来供异步方法调用
+            var callback = function () {
+                return function (err, result) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    return resolve(result);
+                };
+            }
+            args.push(callback());
+            //调用method
+            method.apply(ctx, args);
+        });
+
+    };
+};
