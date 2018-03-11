8f678b7d08c3c79bc945f399d69ea6597d8d19b6
Yurisa <779509411@qq.com>
2018-03-11 19:45:10 +0800

    jquery序列化转json

--- Util.js
+++ Util.js
@@ -138,3 +138,26 @@ function getUrlState(URL) {
     }
     return suc;
 }
+
+
+/**
+ * jquery 序列化表单转化为json数据格式
+ */
+
+$.fn.serializeObject = function()
+            {
+                var o = {};
+                var a = this.serializeArray();
+                $.each(a, function() {
+                    if (o[this.name] !== undefined) {
+                        if (!o[this.name].push) {
+                            o[this.name] = [o[this.name]];
+                        }
+                        o[this.name].push(this.value || '');
+                    } else {
+                        o[this.name] = this.value || '';
+                    }
+                });
+                return o;
+            };
+});
